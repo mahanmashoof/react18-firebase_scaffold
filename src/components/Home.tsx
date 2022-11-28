37a4ebc6a8c530e58fc610b7c54bc6672fe4f951
Mahan Mashoof <mahan.mashoof@gmail.com>
2022-11-28 05:54:07 -0300

    add css

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,15 @@
 import { useFetchTodos } from "../hooks/api/useAPI";
 import { TodoAPI } from "../models/api/FirebaseAPI";
+import "./Home.css";
 
 const Home = () => {
   const { data: todos } = useFetchTodos();
   return (
     <div>
       {todos?.map((todo: TodoAPI, i) => (
-        <div key={i}>
+        <div className="todoDescRow" key={i}>
+          <p>{i + 1}</p>
           <p>{todo.todo}</p>
-          <p>{todo.status}</p>
-          <p>{todo.docId}</p>
         </div>
       ))}
     </div>
