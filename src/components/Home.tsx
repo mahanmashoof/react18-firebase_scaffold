af94e4a8df22a5b5f6793dd49317e2b1c2f76369
Mahan Mashoof <mahan.mashoof@gmail.com>
2022-12-16 03:42:30 -0300

    basic create ok

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,21 @@
 import { useFetchTodos } from "../hooks/api/useAPI";
 import { TodoAPI } from "../models/api/FirebaseAPI";
 import "./Home.css";
+import { collection, addDoc } from "firebase/firestore";
+import firebase from "../firebase";
+import { useRef } from "react";
 
 const Home = () => {
   const { data: todos } = useFetchTodos();
+  let task = useRef<HTMLInputElement>(null);
+  const addNewDoc = async () => {
+    await addDoc(collection(firebase, "todos"), {
+      todo: task.current?.value,
+      created: new Date(),
+      status: 0,
+    });
+  };
+
   return (
     <div>
       {todos?.map((todo: TodoAPI, i) => (
@@ -12,6 +24,8 @@ const Home = () => {
           <p>{todo.todo}</p>
         </div>
       ))}
+      <input ref={task} type="text" placeholder="enter task" />
+      <button onClick={addNewDoc}>create</button>
     </div>
   );
 };
